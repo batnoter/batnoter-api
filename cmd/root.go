5d2c3ca55372a65cb7e93d95dbe9f5947c519b20
vivekweb2013 <vivekweb2013@gmail.com>
2022-03-12 10:47:18 +0530

    Load config from env or file

--- cmd/root.go
+++ cmd/root.go
@@ -3,7 +3,11 @@ package cmd
 import (
 	"os"
 
+	"github.com/iamolegga/enviper"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/vivekweb2013/batnoter/internal/config"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -30,9 +34,29 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.batnoter.yaml)")
+	initConfig()
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	// The enviper is a wrapper over viper that loads the config from file & overrides
+	// them with env variables if available. If the config file is missing it simply ignores it
+	e := enviper.New(viper.New())
+
+	var cfgFile string
+	var conf config.Config
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is .batnoter.yaml)")
+	if cfgFile != "" {
+		e.SetConfigFile(cfgFile)
+	} else {
+		e.AddConfigPath(".")
+		e.SetConfigName(".batnoter")
+	}
+
+	if err := e.Unmarshal(&conf); err == nil {
+		logrus.Infof("Using the config file: %s", e.ConfigFileUsed())
+	}
 }
